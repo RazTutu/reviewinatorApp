2faf91e1e088d64b509b7f79d9ace75e84f86b3a
Stelian Loghin <alexandruloghinstelian121@yahoo.com>
2020-04-07 21:09:43 +0300

    Added NavigationDrawer, fixed some bugs

--- app/src/main/java/com/example/reviewinator/MainActivity.java
+++ app/src/main/java/com/example/reviewinator/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.reviewinator;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.reviewinator.R;
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolbar);
+        navigationView=findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.login:
+                Toast.makeText(MainActivity.this,"Login Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.register:
+                Toast.makeText(MainActivity.this,"Register selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.history:
+                Toast.makeText(MainActivity.this,"History selected",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+
 }
