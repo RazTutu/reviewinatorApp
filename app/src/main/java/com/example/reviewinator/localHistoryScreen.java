b123e720d79d562a9022e679d86078f6be214fab
AndreiPatrascan <45433079+AndreiPatrascan@users.noreply.github.com>
2020-05-06 10:11:38 +0300

    New activity for the local history menu item

---
+++ app/src/main/java/com/example/reviewinator/localHistoryScreen.java
@@ -0,0 +1,12 @@
+package com.example.reviewinator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class localHistoryScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_history_screen);
+    }
+}
